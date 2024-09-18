84ac1e9385f8a02878bb325bc8a5f5cd09526cc1
Mridula Maddukuri <mridulamaddukuri@utexas.edu>
2024-09-18 16:25:39 -0700

    ruff fmt

--- bigram_model.py
+++ bigram_model.py
@@ -5,18 +5,23 @@ For an in-depth explanation, refer to the makemore series on youtube by Andrej K
 """
 
 import torch
-import torch.nn as nn # neural network module
-import torch.nn.functional as F # functional interface
+import torch.nn as nn  # neural network module
+import torch.nn.functional as F  # functional interface
 
-torch.manual_seed(1337) # set seed for reproducibility
+torch.manual_seed(1337)  # set seed for reproducibility
+
 
 class BigramLanguageModel(nn.Module):
     """
     Bigram language model is a simple model that predicts the next token in a sequence given the previous token
     """
+
     def __init__(self, vocab_size):
-        super().__init__() # inherit from nn.Module 
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size) # word embedding table
+        super().__init__()  # inherit from nn.Module
+        # watch bigram video in makemore series
+        self.token_embedding_table = nn.Embedding(
+            vocab_size, vocab_size
+        )  # word embedding table
 
     def forward(self, idx, targets=None):
         """
@@ -26,21 +31,22 @@ class BigramLanguageModel(nn.Module):
         Size of targets, if exists, is also (B, T)
         """
 
-        logits = self.token_embedding_table(idx) # (B, T, C)
+        logits = self.token_embedding_table(idx)  # (B, T, C)
 
         if targets is None:
             loss = None
         else:
             B, T, C = logits.shape
-            # reshape so it is compatible with the cross entropy loss function 
+            # reshape so it is compatible with the cross entropy loss function
             logits = logits.view(B * T, C)
             targets = targets.view(B * T)
             loss = F.cross_entropy(logits, targets)
 
-
         return logits, loss
-    
-    torch.no_grad() # disable gradient computation
+
+    # everything that's hapenning within this function, we will not call .backward() on. No gradients being computed
+    torch.no_grad()  # context manager to disable gradient computation
+
     def generate(self, idx, max_new_tokens):
         """
         Generate new tokens based on the given context.
@@ -57,16 +63,15 @@ class BigramLanguageModel(nn.Module):
             logits, _ = self.forward(idx)
             # logits shape is (B, T, C)
             # Focus only on the last time step
-            logits = logits[:, -1, :] # becomes (B, C)
-            
-            # Apply softmax to get probabilities
-            probs = F.softmax(logits, dim=-1) # (B, C)
-            # Sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
-            # pick the highest prob token
-            
-            # Append sampled index to the running sequence
-            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
-        
-        return idx
+            logits = logits[:, -1, :]  # becomes (B, C)
 
+            # Apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+            # pick the highest prob token
+
+            # Append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
+
+        return idx
--- gpt.py
+++ gpt.py
@@ -10,7 +10,12 @@ This script
 """
 
 import torch
+
 from bigram_model import BigramLanguageModel
+
+# initializations
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 # read the file
@@ -32,8 +37,13 @@ print(vocab_size)
 stoi = {ch: i for i, ch in enumerate(chars)}
 itos = {i: ch for i, ch in enumerate(chars)}
 
-encode = lambda s: [stoi[c] for c in s]
-decode = lambda l: "".join([itos[i] for i in l])
+
+def encode(s):
+    return [stoi[c] for c in s]
+
+
+def decode(enc):
+    return "".join([itos[i] for i in enc])
 
 
 print(encode("welcome"))
@@ -67,8 +77,8 @@ print(val_data[:1000])
 # The model is not typically trained on all the data at once since it is computationally expensive
 # The model will look at a block_size number of characters and predict the next character
 # This is a simple way to handle long sequences
-block_size = 8 # block_size is the number of characters in the input sequence
-print(train_data[:block_size+1])
+block_size = 8  # block_size is the number of characters in the input sequence
+print(train_data[: block_size + 1])
 
 # NOTE:
 # When you sample a chunk of characters like above, it actually has multiple examples
@@ -81,10 +91,10 @@ print(train_data[:block_size+1])
 
 # For illustration
 x = train_data[:block_size]
-y = train_data[1:block_size+1]
+y = train_data[1 : block_size + 1]
 
 for t in range(block_size):
-    context = x[:t+1]
+    context = x[: t + 1]
     target = y[t]
     print(f"when input is {context} the target: {target}")
 
@@ -92,15 +102,18 @@ for t in range(block_size):
 # batch_size is the number of examples in the batch
 # This is done for efficiency because GPUs are great at parallel processing
 batch_size = 4
-# block_size is maximum context length for predictions 
+# block_size is maximum context length for predictions
 block_size = 8
 
+
 # Create a batch of data
-def get_batch(split:str) -> 'tuple[torch.Tensor, torch.Tensor]':
+def get_batch(split: str) -> "tuple[torch.Tensor, torch.Tensor]":
     data = train_data if split == "train" else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x = torch.stack([data[i : i + block_size] for i in ix])
+    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
+    x = x.to(device)
+    y = y.to(device)
     return x, y
 
 
@@ -109,9 +122,9 @@ print(xb.shape, yb.shape)
 print(xb)
 print(yb)
 
-for b in range(batch_size): # batch dimension
-    for t in range(block_size): # time dimension
-        context = xb[b, :t+1]
+for b in range(batch_size):  # batch dimension
+    for t in range(block_size):  # time dimension
+        context = xb[b, : t + 1]
         target = yb[b, t]
         print(f"when input is {context} the target: {target}")
 
@@ -125,25 +138,23 @@ print(logits)
 
 
 # generate new tokens
-idx = torch.zeros((1,1), dtype=torch.long) # (B, T) is (1, 1) here. 
-generated_tokens = m.generate(idx, max_new_tokens=30) # torch.Size([1, 31]
+idx = torch.zeros((1, 1), dtype=torch.long)  # (B, T) is (1, 1) here.
+generated_tokens = m.generate(idx, max_new_tokens=30)  # torch.Size([1, 31]
 print(generated_tokens, generated_tokens.shape)
 
 decoded_tokens = decode(generated_tokens[0].tolist())
 print(decoded_tokens)
 
 
-
 # Training the model
 
 # create a pytorch optimizer
-optimizer = torch.optim.AdamW(m.parameters(), lr = 1e-3)
+optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
 
 step_size = 10000
 batch_size = 32
 for steps in range(step_size):
-
-    xb, yb = get_batch('train')
+    xb, yb = get_batch("train")
 
     # evaluate loss
     logits, loss = m(xb, yb)
@@ -154,13 +165,14 @@ for steps in range(step_size):
 print(loss.item())
 
 # generate new tokens again
-idx = torch.zeros((1,1), dtype=torch.long) # (B, T) is (1, 1) here. 
+idx = torch.zeros((1, 1), dtype=torch.long)  # (B, T) is (1, 1) here.
+
 
 def generate_and_decode(model, idx, num_tokens=30):
-    idx = torch.zeros((1,1), dtype=torch.long)
+    idx = torch.zeros((1, 1), dtype=torch.long)
     generated = model.generate(idx, max_new_tokens=num_tokens)[0]
     return decode(generated.tolist())
 
+
 # Usage
 print(generate_and_decode(m, idx))
-
